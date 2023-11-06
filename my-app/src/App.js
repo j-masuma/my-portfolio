dfa0a9fa38e937a3b2d781c944266df9f5568e73
Javaria Masooma <124299551+j-masuma@users.noreply.github.com>
2023-11-06 18:27:37 +0500

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div>
+      <Header/>
+      <HeroSection/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
